aaa152cd2786e9fc9aee11091c9d5067f659d6a9
Hugo Baba <Hugobarette@gmail.com>
2021-12-27 00:17:06 +0100

    Update buttons when moving or resizing panels

--- ScenePlanning/MainContainer.gd
+++ ScenePlanning/MainContainer.gd
@@ -34,6 +34,7 @@ func _panel_change_column(panel: StreamPanel, column: Column) -> void:
 func _add_panel(panel: StreamPanel, column: Column, dest_pos := Vector2.ZERO, height : float = 100.0) -> void:
 	if !panel.is_connected("column_shift_query", self, "_on_StreamPanel_column_shift_query"):
 		var __ = panel.connect("column_shift_query", self, "_on_StreamPanel_column_shift_query")
+		__ = panel.connect("item_rect_changed", self, "_on_StreamPanel_item_rect_changed", [panel])
 	
 	column.add_child(panel)
 	
@@ -59,16 +60,83 @@ func _generate_add_panel_buttons() -> void:
 			var width = column.rect_size.x 
 			var height = column.rect_size.y / GLOBAL.nb_time_slot
 			var pos = Vector2.DOWN * height * i
-			_add_panel_button(column, Vector2(width, height), pos)
+			_add_panel_button(column, Rect2(pos, Vector2(width, height)))
 
 
-func _add_panel_button(column: Column, size: Vector2, pos: Vector2) -> void:
+func _add_panel_button(column: Column, rect: Rect2) -> void:
 	var add_panel_button = add_panel_button_scene.instance()
 	column.add_child(add_panel_button)
-	add_panel_button.set_size(size)
-	add_panel_button.set_position(pos)
+	add_panel_button.set_size(rect.size)
+	add_panel_button.set_position(rect.position)
 	
 	var __ = add_panel_button.connect("pressed", self, "_on_add_panel_button_pressed", [add_panel_button, column])
+
+
+func _update_column_buttons(column: Column) -> void:
+	var panels_array = _fetch_column_panels(column)
+	var button_array = _fetch_column_buttons(column)
+	
+	# Remove useless buttons & resize hovered ones
+	for panel in panels_array:
+		var panel_rect = panel.get_rect()
+		
+		for button in button_array:
+			var button_rect = button.get_rect()
+			var intersects = panel_rect.intersects(button_rect)
+			var encloses = panel_rect.encloses(button_rect)
+			var same_rect = panel_rect.is_equal_approx(button_rect)
+			
+			if encloses or same_rect:
+				button.queue_free()
+			
+			elif intersects:
+				var button_poly = Utils.rect2poly(button_rect)
+				var panel_poly = Utils.rect2poly(panel_rect)
+				var intersect_poly = Geometry.intersect_polygons_2d(button_poly, panel_poly)[0]
+				var intersect_rect : Rect2 = Utils.poly2rect(intersect_poly)
+				
+				if intersect_rect.has_no_area():
+					button.queue_free()
+				else:
+					button.set_position(intersect_rect.position)
+					button.set_size(intersect_rect.size)
+	
+	var time_slot_height = column.rect_size.y / GLOBAL.nb_time_slot
+	
+	for i in range(GLOBAL.nb_time_slot):
+		var slot_rect = Rect2(Vector2.DOWN * time_slot_height * i, 
+							Vector2(column.rect_size.x, time_slot_height))
+		
+		if _is_time_slot_empty(slot_rect, column):
+			_add_panel_button(column, slot_rect)
+
+
+func _is_time_slot_empty(time_slot_rect: Rect2, column: Column) -> bool:
+	for child in column.get_children():
+		var child_rect = Rect2(child.get_position(), child.get_size())
+		var intersects = child_rect.intersects(time_slot_rect)
+		var encloses = child_rect.encloses(time_slot_rect)
+		var same_rect = child_rect.is_equal_approx(time_slot_rect)
+		
+		if intersects or encloses or same_rect:
+			return false
+	return true
+
+
+func _fetch_column_panels(column: Column) -> Array:
+	var array := Array()
+	for child in column.get_children():
+		if child is StreamPanel:
+			array.append(child)
+	return array
+
+
+func _fetch_column_buttons(column: Column) -> Array:
+	var array := Array()
+	for child in column.get_children():
+		if child is Button:
+			array.append(child)
+	return array
 
 
 #### INPUTS ####
@@ -96,3 +164,10 @@ func _on_add_panel_button_pressed(button: Button, column: Control) -> void:
 	var panel = stream_panel_scene.instance()
 	_add_panel(panel, column, button.rect_position, button.rect_size.y)
 	button.queue_free()
+
+
+func _on_StreamPanel_item_rect_changed(panel: StreamPanel) -> void:
+	if panel.ghost_mode:
+		return
+	
+	_update_column_buttons(panel.get_parent())
